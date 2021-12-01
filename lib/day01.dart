cab85efc7fe6b5b49dcd865b829f4eb656957d86
Jacob Bang <julemand101@archlinux.dk>
2021-12-01 06:30:40 +0100

    Day01: Solved

--- lib/day01.dart
+++ lib/day01.dart
@@ -1,32 +1,35 @@
-// --- Day 1: Report Repair ---
-// https://adventofcode.com/2020/day/1
+// --- Day 1: Sonar Sweep ---
+// https://adventofcode.com/2021/day/1
 
 int solveA(Iterable<String> input) {
-  final list = input.map(int.parse).toList(growable: false);
+  var increases = 0;
+  int? lastDepth;
 
-  for (var a = 0; a < list.length; a++) {
-    for (var b = a + 1; b < list.length; b++) {
-      if (list[a] + list[b] == 2020) {
-        return list[a] * list[b];
-      }
+  for (final depth in input.map(int.parse)) {
+    if (lastDepth != null && lastDepth < depth) {
+      increases++;
     }
+
+    lastDepth = depth;
   }
 
-  throw Exception('No solution found!');
+  return increases;
 }
 
 int solveB(Iterable<String> input) {
-  final list = input.map(int.parse).toList(growable: false);
+  final depths = input.map(int.parse).toList(growable: false);
+  var increases = 0;
+  int? lastDepth;
 
-  for (var a = 0; a < list.length; a++) {
-    for (var b = a + 1; b < list.length; b++) {
-      for (var c = b + 1; c < list.length; c++) {
-        if (list[a] + list[b] + list[c] == 2020) {
-          return list[a] * list[b] * list[c];
-        }
-      }
+  for (var i = 0; i < depths.length - 2; i++) {
+    final depth = depths[i] + depths[i + 1] + depths[i + 2];
+
+    if (lastDepth != null && lastDepth < depth) {
+      increases++;
     }
+
+    lastDepth = depth;
   }
 
-  throw Exception('No solution found!');
+  return increases;
 }
--- test/day01_test.dart
+++ test/day01_test.dart
@@ -1,5 +1,5 @@
-// --- Day 1: Report Repair ---
-// https://adventofcode.com/2020/day/1
+// --- Day 1: Sonar Sweep ---
+// https://adventofcode.com/2021/day/1
 
 import 'dart:io';
 import 'package:advent_of_code_2021/day01.dart';
@@ -10,20 +10,44 @@ final input = File('test/data/day01.txt').readAsLinesSync();
 void main() {
   group('Part One', () {
     test('Example 1', () {
-      expect(solveA(const ['1721', '979', '366', '299', '675', '1456']),
-          equals(514579));
+      expect(
+          solveA(const [
+            '199',
+            '200',
+            '208',
+            '210',
+            '200',
+            '207',
+            '240',
+            '269',
+            '260',
+            '263',
+          ]),
+          equals(7));
     });
     test('Solution', () {
-      expect(solveA(input), equals(972576));
+      expect(solveA(input), equals(1390));
     });
   });
   group('Part Two', () {
     test('Example 1', () {
-      expect(solveB(const ['1721', '979', '366', '299', '675', '1456']),
-          equals(241861950));
+      expect(
+          solveB(const [
+            '199',
+            '200',
+            '208',
+            '210',
+            '200',
+            '207',
+            '240',
+            '269',
+            '260',
+            '263',
+          ]),
+          equals(5));
     });
     test('Solution', () {
-      expect(solveB(input), equals(199300880));
+      expect(solveB(input), equals(1457));
     });
   });
 }
