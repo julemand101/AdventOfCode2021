89d393cd562a2e10af44e30953fdae0f364f4d1b
Jacob Bang <julemand101@archlinux.dk>
2021-12-19 23:38:31 +0100

    Day14: Solved

--- lib/day14.dart
+++ lib/day14.dart
@@ -1,49 +1,76 @@
 // --- Day 14: Extended Polymerization ---
 // https://adventofcode.com/2021/day/14
 
-import 'dart:collection';
-
 import 'dart:math';
 
-int solveA(List<String> input) {
-  final polymerTemplate = LinkedList<LetterEntry>()
-    ..addAll(input.first.split('').map((letter) => LetterEntry(letter)));
-  final pairInsertionRules = <String, Map<String, String>>{};
+int solveA(List<String> input) => solve(input, steps: 10);
+int solveB(List<String> input) => solve(input, steps: 40);
+
+int solve(List<String> input, {required int steps}) {
+  var pairCount = <String, int>{};
+  for (final pair in input.first.pairs) {
+    pairCount.addInt(pair, 1);
+  }
+
+  final letterCountMap = <String, int>{};
+  for (final letter in input.first.letters) {
+    letterCountMap.addInt(letter, 1);
+  }
+
+  final pairInsertionRules = <String, LetterPair>{};
 
   for (final line in input.skip(2)) {
     // Example of line:
     // CH -> B
     // 0123456 <- index position in line
-    pairInsertionRules
-        .putIfAbsent(line[0], () => {})
-        .putIfAbsent(line[1], () => line[6]);
+    pairInsertionRules[line.substring(0, 2)] = LetterPair(line);
   }
 
-  for (var step = 0; step < 10; step++) {
-    for (var letterEntry = polymerTemplate.first.next;
-        letterEntry != null;
-        letterEntry = letterEntry.next) {
-      final letterToInsert =
-          pairInsertionRules[letterEntry.previous!.letter]?[letterEntry.letter];
-      if (letterToInsert != null) {
-        letterEntry.insertBefore(LetterEntry(letterToInsert));
-      }
+  for (var step = 0; step < steps; step++) {
+    final newPairCount = <String, int>{};
+
+    for (final entry in pairCount.entries) {
+      final pair = entry.key;
+      final count = entry.value;
+      final pairInsertionRule = pairInsertionRules[pair]!;
+
+      newPairCount
+        ..addInt(pairInsertionRule.firstPair, count)
+        ..addInt(pairInsertionRule.secondPair, count);
+      letterCountMap.addInt(pairInsertionRule.insertedLetter, count);
     }
-  }
 
-  final letterCountMap = polymerTemplate.fold<Map<String, int>>(
-      <String, int>{},
-      (map, letterEntry) => map
-        ..update(letterEntry.letter, (value) => value + 1, ifAbsent: () => 1));
+    pairCount = newPairCount;
+  }
 
   return letterCountMap.values.reduce(max) - letterCountMap.values.reduce(min);
 }
 
-class LetterEntry extends LinkedListEntry<LetterEntry> {
-  final String letter;
+class LetterPair {
+  final String firstPair;
+  final String secondPair;
+  final String insertedLetter;
 
-  LetterEntry(this.letter);
+  // Example of line:
+  // CH -> B
+  // 0123456 <- index position in line
+  LetterPair(String line)
+      : firstPair = line[0] + line[6],
+        secondPair = line[6] + line[1],
+        insertedLetter = line[6];
+}
 
-  @override
-  String toString() => letter;
+extension StringExtension on String {
+  Iterable<String> get pairs sync* {
+    for (var i = 1; i < length; i++) {
+      yield substring(i - 1, i + 1);
+    }
+  }
+
+  Iterable<String> get letters => split('');
+}
+
+extension MapExtension on Map<String, int> {
+  void addInt(String key, int addValue) =>
+      update(key, (value) => value + addValue, ifAbsent: () => addValue);
 }
--- test/day14_test.dart
+++ test/day14_test.dart
@@ -37,4 +37,33 @@ void main() {
       expect(solveA(input), equals(4517));
     });
   });
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(
+          solveB(const [
+            'NNCB',
+            '',
+            'CH -> B',
+            'HH -> N',
+            'CB -> H',
+            'NH -> C',
+            'HB -> C',
+            'HC -> B',
+            'HN -> C',
+            'NN -> C',
+            'BH -> H',
+            'NC -> B',
+            'NB -> B',
+            'BN -> B',
+            'BB -> N',
+            'BC -> B',
+            'CC -> N',
+            'CN -> C',
+          ]),
+          equals(2188189693529));
+    });
+    test('Solution', () {
+      expect(solveB(input), equals(4704817645083));
+    });
+  });
 }
