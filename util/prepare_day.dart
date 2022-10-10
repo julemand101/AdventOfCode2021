f6611a55a7deb1131f24e6b1747b6ef5ca5eedd1
Jacob Bang <julemand101@archlinux.dk>
2022-10-10 10:45:31 +0200

    Prepare prepare_day script for reusage for next years

--- util/prepare_day.dart
+++ util/prepare_day.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+const year = '2021';
+
 void main(List<String> args) {
   if (args.length != 2) {
     print('Please call with: <dayNumber> <dayTitle>');
@@ -16,7 +18,7 @@ void main(List<String> args) {
   File('lib/$dayFileName').writeAsString(
     '''
 // --- Day $dayNumber: $dayTitle ---
-// https://adventofcode.com/2021/day/$dayNumber
+// https://adventofcode.com/$year/day/$dayNumber
 
 int solveA(Iterable<String> input) {
   return 0;
@@ -34,10 +36,10 @@ int solveA(Iterable<String> input) {
   ).writeAsString(
     '''
 // --- Day $dayNumber: $dayTitle ---
-// https://adventofcode.com/2021/day/$dayNumber
+// https://adventofcode.com/$year/day/$dayNumber
 
 import 'dart:io';
-import 'package:advent_of_code_2021/$dayFileName';
+import 'package:advent_of_code_$year/$dayFileName';
 import 'package:test/test.dart';
 
 final input = File('$dataPath').readAsLinesSync();
